a5703a8ea11c33721aa6cf89cfc63ffc26e9c846
rsoucy <rsoucy@lifeimage.com>
2015-06-18 16:24:43 -0400

    Update MyApplicationToTest.java

--- src/main/java/net/continuumsecurity/examples/ropeytasks/MyApplicationToTest.java
+++ src/main/java/net/continuumsecurity/examples/ropeytasks/MyApplicationToTest.java
@@ -29,7 +29,7 @@ public class MyApplicationToTest extends WebApplication implements ILogin,
     @Override
     public void openLoginPage() {
         driver.get(Config.getInstance().getBaseUrl() + "/universal-inbox/login");
-        verifyTextPresent("Login");
+        verifyTextPresent("login");
     }
 
   @Override
