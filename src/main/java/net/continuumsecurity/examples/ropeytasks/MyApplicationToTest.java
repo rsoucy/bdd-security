8f2e96c3ec96b888e24a896136419bbf826f0512
rsoucy <rsoucy@lifeimage.com>
2015-06-18 12:15:50 -0400

    Update MyApplicationToTest.java

--- src/main/java/net/continuumsecurity/examples/ropeytasks/MyApplicationToTest.java
+++ src/main/java/net/continuumsecurity/examples/ropeytasks/MyApplicationToTest.java
@@ -19,7 +19,8 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
 
-public class MyApplicationToTest extends WebApplication implements ILogin,ILogout {
+public class MyApplicationToTest extends WebApplication implements ILogin, 
+          ILogout {
      
       public MyApplicationToTest() {
         super();
