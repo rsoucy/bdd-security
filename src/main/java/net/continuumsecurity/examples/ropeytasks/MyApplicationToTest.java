2ece853153ece3ffa0414e8c25ef079c6987a10f
Roger Soucy <rsoucy@lifeimage.com>
2015-06-18 11:01:26 -0400

    Adding UBox test

---
+++ src/main/java/net/continuumsecurity/examples/ropeytasks/MyApplicationToTest.java
@@ -0,0 +1,15 @@
+public class MyApplicationToTest extends WebApplication {
+
+    public class RopeyTasksApplication() {
+         super();
+    }
+
+    public void navigate() {
+        driver.get(baseUrl + "/universal-inbox/login”);
+        driver.findElement(By.id("login_username")).clear();
+        driver.findElement(By.id("login_username")).sendKeys("donotreply+1515@lifeimage.com”);
+        driver.findElement(By.id("login_password")).clear();
+        driver.findElement(By.id("login_password")).sendKeys("lifeimage1_new”);
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("Log out")).click();
+    }   
