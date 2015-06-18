e1ca87db3ad11e44429fc54a053992d5498268f7
rsoucy <rsoucy@lifeimage.com>
2015-06-18 11:30:32 -0400

    Update MyApplicationToTest.java

--- src/main/java/net/continuumsecurity/examples/ropeytasks/MyApplicationToTest.java
+++ src/main/java/net/continuumsecurity/examples/ropeytasks/MyApplicationToTest.java
@@ -25,7 +25,7 @@ public class MyApplicationToTest extends WebApplication {
     public void navigate() {
         driver.get(Config.getInstance().getBaseUrl());
         driver.findElement(By.id("login_username")).clear();
-        driver.findElement(By.id("login_username")).sendKeys("donotreply+1515@lifeimage.com”);
+        driver.findElement(By.id("login_username")).sendKeys("donotreply1515@lifeimage.com”);
         driver.findElement(By.id("login_password")).clear();
         driver.findElement(By.id("login_password")).sendKeys("lifeimage1_new”);
         driver.findElement(By.name("submit")).click();
