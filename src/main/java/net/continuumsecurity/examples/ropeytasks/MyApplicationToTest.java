720ff3aee19d96bff8b0be8391a8a0cb1e6488ff
rsoucy <rsoucy@lifeimage.com>
2015-06-18 14:45:38 -0400

    Update MyApplicationToTest.java

--- src/main/java/net/continuumsecurity/examples/ropeytasks/MyApplicationToTest.java
+++ src/main/java/net/continuumsecurity/examples/ropeytasks/MyApplicationToTest.java
@@ -19,45 +19,68 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
 
-public class MyApplicationToTest extends WebApplication implements ILogin {
-     
-      public MyApplicationToTest() {
-        super();
+public class MyApplicationToTest {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://dev-cloud-auto2:7070/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testMy() throws Exception {
+    driver.get(baseUrl + "/universal-inbox/login");
+    driver.findElement(By.id("login_username")).clear();
+    driver.findElement(By.id("login_username")).sendKeys("donotreply+1515@lifeimage.com");
+    driver.findElement(By.id("login_password")).clear();
+    driver.findElement(By.id("login_password")).sendKeys("lifeimage1_new");
+    driver.findElement(By.name("submit")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
     }
-     
-     @Override
-     public void openLoginPage() {
-        driver.get(Config.getInstance().getBaseUrl() + "/universal-inbox/login");
-        verifyTextPresent("login");
-     }
+  }
 
-     @Override
-     public void login(Credentials credentials) {
-        UserPassCredentials creds = new UserPassCredentials(credentials);
-        //driver.findElement(By.id("login_username")).clear();
-        driver.findElement(By.id("login_username")).sendKeys("donotreply+1515@lifeimage.com");
-        //driver.findElement(By.id("login_password")).clear();
-        driver.findElement(By.id("login_password")).sendKeys("lifeimage1_new");
-        driver.findElement(By.name("submit")).click();
-     }
-     
-     // Convenience method
-     public void login(String username, String password) {
-          login(new UserPassCredentials(username, password));
-     }
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
 
-     @Override
-     public boolean isLoggedIn() {
-        if (driver.getPageSource().contains("Tasks")) {
-            return true;
-        } else {
-            return false;
-        }
-     }
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
 
-     public void navigate() {
-        openLoginPage();
-        login(Config.getInstance().getUsers().getDefaultCredentials());
-        //navigate the app
-     }
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
 }
